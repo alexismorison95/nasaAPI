be079df6f1e47c64120481916c928226a43c3817
alexismorison95 <alexismorison83@gmail.com>
2020-06-26 17:49:39 -0300

    first commit

---
+++ NasaAPI/MainPage.xaml.cs
@@ -0,0 +1,155 @@
+﻿using NasaAPI.Pages;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace NasaAPI
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static MainPage Current;
+
+        public ObservableCollection<MyPage> pages = new ObservableCollection<MyPage>
+        {
+            new MyPage() { Tag="Home", Tooltip="Imagen del día", Glyph=Symbol.Home, ClassType=typeof(Home) },
+            new MyPage() { Tag="AcercaDe", Tooltip="Acerca de", Glyph=Symbol.ContactInfo, ClassType=typeof(AcercaDe) },
+            //new MyPage() { Tag="Playlists", Tooltip="Listas de reproduccion", Glyph=Symbol.MusicInfo, ClassType=typeof(Playlists) }
+            //new Scenario() { Name="Settings", Tooltip="Configuracion", Glyph=Symbol.Home, ClassType=typeof(Settings) }
+        };
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Current = this;
+
+            CustomTitleBar();
+        }
+
+        private void CustomTitleBar()
+        {
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            titleBar.ButtonForegroundColor = Colors.White;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonHoverForegroundColor = Colors.White;
+            titleBar.ButtonHoverBackgroundColor = Color.FromArgb(30, 255, 255, 255);
+            titleBar.ButtonPressedForegroundColor = Colors.White;
+            titleBar.ButtonPressedBackgroundColor = Colors.DodgerBlue;
+
+            titleBar.ButtonInactiveForegroundColor = Colors.Gray;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+        }
+
+        private void navigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                NavViewNavigate("settings", args.RecommendedNavigationTransitionInfo);
+            }
+            else if (args.SelectedItemContainer != null)
+            {
+                var navItemName = args.SelectedItemContainer.Tag.ToString();
+
+                NavViewNavigate(navItemName, args.RecommendedNavigationTransitionInfo);
+            }
+        }
+
+        private void NavViewNavigate(string navItemName, NavigationTransitionInfo transitionInfo)
+        {
+            Type _page = null;
+
+            if (navItemName == "settings")
+            {
+                _page = typeof(Settings);
+            }
+            else
+            {
+                var item = pages.FirstOrDefault(p => p.Tag.Equals(navItemName));
+                _page = item.ClassType;
+            }
+
+            var preNavPageType = contentFrame.CurrentSourcePageType;
+
+            if (!(_page is null) && !Type.Equals(preNavPageType, _page))
+            {
+                contentFrame.Navigate(_page, null, transitionInfo);
+            }
+        }
+
+        private void navigationView_Loaded(object sender, RoutedEventArgs e)
+        {
+            contentFrame.Navigated += ContentFrame_Navigated;
+
+            navigationView.SelectedItem = navigationView.MenuItems[0];
+
+            NavViewNavigate("Home", new EntranceNavigationTransitionInfo());
+        }
+
+        private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            navigationView.IsBackEnabled = contentFrame.CanGoBack;
+
+            if (contentFrame.SourcePageType == typeof(Settings))
+            {
+                // SettingsItem is not part of NavView.MenuItems, and doesn't have a Tag.
+                navigationView.SelectedItem = (NavigationViewItem)navigationView.SettingsItem;
+                navigationView.Header = "Configuración";
+            }
+            else if (contentFrame.SourcePageType != null)
+            {
+                var item = pages.FirstOrDefault(p => p.ClassType == e.SourcePageType);
+
+                navigationView.SelectedItem = navigationView.MenuItems
+                    .OfType<NavigationViewItem>()
+                    .First(n => n.Tag.Equals(item.Tag));
+
+                navigationView.Header = ((NavigationViewItem)navigationView.SelectedItem)?.Content?.ToString();
+            }
+        }
+
+        private void navigationView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (contentFrame.CanGoBack)
+            {
+                contentFrame.GoBack();
+            }
+        }
+    }
+
+    public class MyPage
+    {
+        public string Tag { get; set; }
+
+        public string Tooltip { get; set; }
+
+        public Symbol Glyph { get; set; }
+
+        public Type ClassType { get; set; }
+    }
+}
---
+++ NasaAPI/Pages/Home.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Json;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.System.UserProfile;
+using Windows.Storage;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Windows.UI;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace NasaAPI.Pages
+{
+    /// <summary>
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class Home : Page
+    {
+        string dataURL = "https://api.nasa.gov/planetary/apod?api_key=XdRrmURyk5bW91jnAyoHbaAngJrF8vKIiQiZI6AV";
+
+        string fondoRuta = "ms-appdata:///local/DownloadedImages/fondo";
+
+        NasaJSON nasa;
+
+        StorageFile fileFondo;
+        bool isDescargandoFondo = true;
+
+        public Home()
+        {
+            this.InitializeComponent();
+
+            LoadData();
+        }
+
+        private async void LoadData()
+        {
+            var json = new WebClient().DownloadString(dataURL);
+
+            nasa = JsonConvert.DeserializeObject<NasaJSON>(json);
+
+            textBlockDescripcion.Text = nasa.explanation;
+
+            textBlockTitulo.Text = nasa.title;
+
+            if (nasa.copyright != null)
+            {
+                textBlockAutor.Text = nasa.copyright;
+            }
+            else
+            {
+                textBlockAutor.Text = "";
+            }
+
+            // Descargo la vista preliminar
+
+            Uri uri = new Uri(nasa.url);
+            ImageSource imgSource = new BitmapImage(uri);
+            imageVista.Source = imgSource;
+
+            progressImage.IsActive = false;
+
+            // Descargo el wallpaper
+
+            if (isDescargandoFondo)
+            {
+                progressImage2.IsActive = true;
+                textBlockWallpaper.Visibility = Visibility.Visible;
+
+                string imagenString = await DownloadImage(nasa.hdurl, "fondo");
+                Uri uriFondo = new Uri(imagenString);
+
+                fileFondo = await StorageFile.GetFileFromApplicationUriAsync(uriFondo);
+
+                isDescargandoFondo = false;
+                progressImage2.IsActive = false;
+                textBlockWallpaper.Visibility = Visibility.Collapsed;
+
+                btnAplicar.IsEnabled = true;
+            } 
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (UserProfilePersonalizationSettings.IsSupported())
+            {
+                //LoadingControl.IsLoading = true;
+                //progressRing.IsActive = true;
+
+                UserProfilePersonalizationSettings profileSettings = UserProfilePersonalizationSettings.Current;
+
+                await profileSettings.TrySetWallpaperImageAsync(fileFondo);
+
+                //LoadingControl.IsLoading = false;
+                //progressRing.IsActive = false;
+            }
+        }
+
+        private async Task<String> DownloadImage(string url, String fileName)
+        {
+            const String imagesSubdirectory = "DownloadedImages";
+
+            var rootFolder = await ApplicationData.Current.LocalFolder.CreateFolderAsync(imagesSubdirectory, CreationCollisionOption.OpenIfExists);
+
+            var storageFile = await rootFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+
+            using (HttpClient client = new HttpClient())
+            {
+                byte[] buffer = await client.GetByteArrayAsync(url);
+                using (Stream stream = await storageFile.OpenStreamForWriteAsync())
+                    stream.Write(buffer, 0, buffer.Length);
+            }
+
+            // Use this path to load image
+            String newPath = String.Format("ms-appdata:///local/{0}/{1}", imagesSubdirectory, fileName);
+
+            return newPath;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    public class NasaJSON
+    {
+        public string copyright { get; set; }
+        public string date { get; set; }
+        public string explanation { get; set; }
+        public string hdurl { get; set; }
+        public string media_type { get; set; }
+        public string service_version { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+    }
+}
